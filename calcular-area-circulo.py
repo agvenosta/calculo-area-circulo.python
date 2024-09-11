8ae90655dcac3cad628504d1631c20085d21255e
agvenosta <agvenosta@gmail.com>
2024-09-11 15:20:43 -0300

    Realizamos una calculadora de area de un circulo para poder practicar algoritmos, pseudocodigo y diagrama de flujo

---
+++ calcular-area-circulo.py
@@ -0,0 +1,14 @@
+# Paso 1: Solicitar al usuario que ingrese el radio del circulo que quiere calcular.
+
+import math
+
+
+radio = float(input("Por favor, ingrese el radio del circulo: "))
+
+# Paso 2: Calcular el area del circulo utilizando la formula area = π * radio^2.
+
+area = math.pi * (radio**2)
+
+# Paso 3: Mostrar al usuario el area calculada.
+
+print("El area del circulo con radio", radio, "es: ", area)
